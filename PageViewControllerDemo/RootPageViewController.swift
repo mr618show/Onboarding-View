8d6a988672b7c7211c4348be79699ee1da6928bc
Rui Mao <mr618show@hotmail.com>
2018-04-20 16:29:44 -0700

    add labels and animations

--- PageViewControllerDemo/RootPageViewController.swift
+++ PageViewControllerDemo/RootPageViewController.swift
@@ -21,27 +21,48 @@ class RootPageViewController: UIPageViewController, UIPageViewControllerDataSour
         let vc5 = sb.instantiateViewController(withIdentifier: "chatVC")
         
         return [vc1, vc2, vc3, vc4, vc5]
+
     }()
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Futura", size: 40)
+        return label
+    }()
+    
+    lazy var bodyLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    
+    lazy var stackView = UIStackView(arrangedSubviews: [self.titleLabel, self.bodyLabel])
+    lazy var content: [(title: String, description: String)] = [
+        ("Welcome to Gratitudine", "Your everyday personal gratitude journal" ),
+        ("Customize your own journal book", "Tired of looking at the same background everyday? Try our custom themes that match your mood everyday!🌈"),
+        ("Safty is important", "Now you can set up a password or using touchID to make your journal private 🔐"),
+        ("Have questions?","We are here to help! 🙋‍♂️🙋 In the meantime we welcome all the suggestion that will make Gratitudine better."),
+        ("We are grateful", "Thanks so much for downloading our app and giving us a try. Make sure to leave us a good review in the AppStore!❤️")
+    ]
+    
+    private var currentPage = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         self.dataSource = self
-        
+        setupGestures()
+        setupLabels(withPage: currentPage)
         if let firstViewController = viewControllerLists.first {
             self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
-            
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         guard let vcIndex = viewControllerLists.index(of: viewController) else {return nil}
+        setupLabels(withPage: currentPage)
         let previousIndex = vcIndex - 1
         guard previousIndex >= 0 else {return nil}
         guard viewControllerLists.count > previousIndex else {return nil}
@@ -49,6 +70,7 @@ class RootPageViewController: UIPageViewController, UIPageViewControllerDataSour
     }
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         guard let vcIndex = viewControllerLists.index(of: viewController) else {return nil}
+        setupLabels(withPage: currentPage)
         let nextIndex = vcIndex + 1
         guard viewControllerLists.count != nextIndex else {return nil}
         guard viewControllerLists.count > nextIndex else {return nil}
@@ -63,6 +85,64 @@ class RootPageViewController: UIPageViewController, UIPageViewControllerDataSour
         appearance.backgroundColor = UIColor.darkGray
     }
     
+    fileprivate func setupLabels(withPage page: Int){
+        let appearance = UIPageControl.appearance()
+        appearance.currentPage = page
+        titleLabel.text = content[page].title
+        bodyLabel.text = content[page].description
+        titleLabel.numberOfLines = 0
+        bodyLabel.numberOfLines = 0
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 8
+        if !stackView.isDescendant(of: view){
+            view.addSubview(stackView)
+        }
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -120).isActive = true
+        self.view.layoutIfNeeded()
+        
+    }
+    func setupGestures(){
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapAnimations))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc fileprivate func handleTapAnimations(){
+        //first animation
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.titleLabel.alpha = 0.8
+            self.titleLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+        }) { (_) in
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+                self.titleLabel.alpha = 0
+                self.titleLabel.transform = CGAffineTransform(translationX: 0, y: -550)
+            }, completion: nil)
+        }
+        //second animation
+        UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.bodyLabel.alpha = 0.8
+            self.bodyLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+        }) { (_) in
+            UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+                self.bodyLabel.alpha = 0
+                self.bodyLabel.transform = CGAffineTransform(translationX: 0, y: -550)
+            }, completion: { _ in
+                self.stackView.removeFromSuperview()
+                self.titleLabel.alpha = 1
+                self.titleLabel.transform = .identity
+                self.bodyLabel.alpha = 1
+                self.bodyLabel.transform = .identity
+                self.currentPage += 1
+//                if self.currentPage < self.content.count {
+//                    self.setupLabels(withPage: self.currentPage)
+//                }
+            })
+        }
+        
+    }
+    
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
         setupPageControl()
         return self.viewControllerLists.count
@@ -72,14 +152,7 @@ class RootPageViewController: UIPageViewController, UIPageViewControllerDataSour
         return 0
     }
     
-    /*func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
-        return viewControllerLists.count
-    }
-    
-    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
-        return 0
-    }
- */
+
 
     /*
     // MARK: - Navigation
